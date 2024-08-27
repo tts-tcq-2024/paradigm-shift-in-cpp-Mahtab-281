45731832791f76282e03db52a7663e8ebf6426fa
Mahtab Ahmad <105929699+Mahtab-281@users.noreply.github.com>
2024-08-27 11:54:39 +0530

    Mahtab-281_Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,18 +2,40 @@
 #include <iostream>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
+void printOutOfRange(const string& parameter){
+  cout << parameter << " out of range!\n";
+}
+
+bool isValueInRange(float minimum, float maximum, float value, const string& parameter)
+{
+  if (value < minimum || value > maximum)
+  {
+    printOutOfRange(parameter);
     return false;
   }
   return true;
+}
+
+bool isValueGreaterThan(float maximum, float value, const string& parameter)
+{
+  if (value > maximum)
+  {
+    printOutOfRange(parameter);
+    return false;
+  }
+  return true;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+  bool temperatureStatus = false;
+  bool socStatus = false;
+  bool chargeRateStatus = false;
+
+  temperatureStatus = isValueInRange(0, 45, temperature, "Temperature");
+  socStatus = isValueInRange(20, 80, soc, "State of Charge");
+  chargeRateStatus = isValueGreaterThan(0.8, chargeRate, "Charge Rate");
+
+  return temperatureStatus && socStatus && chargeRateStatus;
 }
 
 int main() {
